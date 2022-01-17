e0932a220827dcba976d8eeda8a14d707ffd75f1
Joey Wessel <joey.wessel@gmail.com>
2022-01-16 21:51:16 -0500

    Test project

---
+++ Test-SFMarketingCloudSDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test-SFMarketingCloudSDK
+//
+//  Created by Joey Wessel on 1/16/22.
+//
+
+import UIKit
+import MarketingCloudSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
